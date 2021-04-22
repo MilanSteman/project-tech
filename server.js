4fbfbf1575f16478ecca27cf8c335b4709721a36
MilanSteman <milansteman@gmail.com>
2021-04-22 12:21:31 +0200

    Added Nunjucks, set up layout and partials

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const nunjucks = require('nunjucks');
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+app.get('/', function(req, res) {
+  let data = {
+    message: 'Hello world!',
+    title: 'Home',
+    authorised: false,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque varius hendrerit gravida. Donec at pharetra ex. Aliquam vel posuere libero. In id enim ut enim fermentum vehicula quis eu ante.'
+  }
+
+  res.render('home.njk', data)
+});
+
+//middelware
+app.use(express.static('static/public'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/movies', (req, res) => {
+  res.send('<h1>This will render a list of movies.</h1>')
+})
+
+app.get('/movies/:movieId/:slug', (req, res) => {
+  res.send(`<h1>This will render a detail page for ${req.params.slug}`)
+})
+
+//middleware
+app.use(function (req, res, next) {
+  res.status(404).send("<h1>Sorry, we can't find that page!</h1>")
 })
 
 app.listen(port, () => {
