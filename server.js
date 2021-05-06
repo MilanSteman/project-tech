5e39ce89cdd72bbb36d3e1fdb68df74cef3f850d
MilanSteman <milansteman@gmail.com>
2021-05-06 14:45:40 +0200

    Added ESLint

--- server.js
+++ server.js
@@ -1,103 +1,105 @@
-const express = require("express");
+/* eslint-disable prettier/prettier */
+const express = require('express');
+const nunjucks = require('nunjucks');
+
 const app = express();
 const port = 3000;
-const nunjucks = require("nunjucks");
 
 const data = {
-  message: "Welkom terug, milan.",
-  displayname: "milan",
-  username: "milannn",
-  title: "Home",
-  picture: "images/profile-picture.jpg",
-  banner: "images/michael.jpg",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui eros, porta ut urna in, lacinia consectetur nisl.",
+  message: 'Welkom terug, milan.',
+  displayname: 'milan',
+  username: 'milannn',
+  title: 'Home',
+  picture: 'images/profile-picture.jpg',
+  banner: 'images/michael.jpg',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dui eros, porta ut urna in, lacinia consectetur nisl.',
 
-  posts: ["images/michael.jpg", "images/michael.jpg", "images/michael.jpg"],
+  posts: ['images/michael.jpg', 'images/michael.jpg', 'images/michael.jpg'],
 };
 
 // temporary array for users
 const users = [
   {
-    username: "Simeon Yetarian",
-    picture: "images/simeon.png",
-    description: "Go and get it. Just try to bring the car back in good condition huh?",
-    games: ["GTA V", "GTA IV"],
+    username: 'Simeon Yetarian',
+    picture: 'images/simeon.png',
+    description: 'Go and get it. Just try to bring the car back in good condition huh?',
+    games: ['GTA V', 'GTA IV'],
   },
   {
-    username: "Franklin Clinton",
-    picture: "images/franklin.jpg",
-    description: "A what? A credit fraud? Be serious dude.... I just work the repo man.",
-    games: ["GTA V", "GTA San Andreas"],
+    username: 'Franklin Clinton',
+    picture: 'images/franklin.jpg',
+    description: 'A what? A credit fraud? Be serious dude.... I just work the repo man.',
+    games: ['GTA V', 'GTA San Andreas'],
   },
   {
-    username: "Michael Townley",
-    picture: "images/michael.jpg",
-    description: "You forget a thousand things everyday, pal. Make sure this is one of them.",
-    games: ["GTA V", "GTA San Andreas"],
+    username: 'Michael Townley',
+    picture: 'images/michael.jpg',
+    description: 'You forget a thousand things everyday, pal. Make sure this is one of them.',
+    games: ['GTA V', 'GTA San Andreas'],
   },
 ];
 
 // temporary array for users
 const onlineUsers = [
   {
-    username: "Franklin Clinton",
-    picture: "images/franklin.jpg",
+    username: 'Franklin Clinton',
+    picture: 'images/franklin.jpg',
     online: true
   },
   {
-    username: "Michael Townley",
-    picture: "images/michael.jpg",
+    username: 'Michael Townley',
+    picture: 'images/michael.jpg',
     online: false
   },
   {
-    username: "Simeon Yetarian",
-    picture: "images/simeon.png",
+    username: 'Simeon Yetarian',
+    picture: 'images/simeon.png',
     online: true
   },
 ];
 
-nunjucks.configure("views", {
+nunjucks.configure('views', {
   autoescape: true,
   express: app,
 });
 
-app.get("/", (req, res) => {
-  res.render("home.njk", { data, users, onlineUsers });
+app.get('/', (req, res) => {
+  res.render('home.njk', { data, users, onlineUsers });
 });
 
-app.get("/profile", (req, res) => {
-  res.render("profile.njk", { data, users, onlineUsers });
+app.get('/profile', (req, res) => {
+  res.render('profile.njk', { data, users, onlineUsers });
 });
 
-app.get("/settings/profile", (req, res) => {
-  res.render("profile-settings.njk", { data });
+app.get('/settings/profile', (req, res) => {
+  res.render('profile-settings.njk', { data });
 });
 
-//middelware
-app.use(express.static("static/public"));
+// middelware
+app.use(express.static('static/public'));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.get("/movies", (req, res) => {
-  res.send("<h1>This will render a list of movies.</h1>");
+app.get('/movies', (req, res) => {
+  res.send('<h1>This will render a list of movies.</h1>');
 });
 
-app.get("/movies/:movieId/:slug", (req, res) => {
+app.get('/movies/:movieId/:slug', (req, res) => {
   res.send(`<h1>This will render a detail page for ${req.params.slug}`);
 });
 
-//middleware
+// middleware
 app.use((req, res, next) => {
-  let data = {
-    message: "Error 404",
-    title: "Error 404",
+  const data2 = {
+    message: 'Error 404',
+    title: 'Error 404',
     authorised: true,
-    description: "We could not find that page!",
+    description: 'We could not find that page!',
   };
 
-  res.status(404).render("home.njk", data);
+  res.status(404).render('home.njk', data2);
 });
 
 app.listen(port, () => {
